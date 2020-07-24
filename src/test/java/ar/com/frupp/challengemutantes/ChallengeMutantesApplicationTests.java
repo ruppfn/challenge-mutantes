0725c5d2e8046d7678797588dfdf89969d7048fd
bshyn <52586606+bshyn@users.noreply.github.com>
2020-07-24 20:53:35 -0300

    Spring Initializr

---
+++ src/test/java/ar/com/frupp/challengemutantes/ChallengeMutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.frupp.challengemutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMutantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
